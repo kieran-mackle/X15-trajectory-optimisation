b99eb5165f5d77cd36e313eb196a91139e7c508b
Kieran <kemackle98@gmail.com>
2021-10-05 15:20:16 +1000

    created earth radius function

---
+++ functions/utilities/GetRe.m
@@ -0,0 +1,10 @@
+function Re = GetRe(latitude, model)
+if model == 1
+    Re = 6378137.0;
+elseif model == 2
+    a = 6378137.0;
+    f = 3.33528106e-3;
+    Re = a.*(1-(f/2).*(1-cos(2.*latitude)) + ...
+              (5.*f.^2./16).*(1-cos(4.*latitude)));
+else
+end
