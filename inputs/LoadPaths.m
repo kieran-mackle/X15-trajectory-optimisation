1f92077e5e5328b49964ea1d89b12cdf2084c051
Kieran <kemackle98@gmail.com>
2021-10-05 11:30:26 +1000

    moved path load script

---
+++ inputs/LoadPaths.m
@@ -0,0 +1,8 @@
+addpath ../aero/
+addpath ../functions/
+addpath ../functions/objectives/
+addpath ../functions/transformations/
+addpath ../functions/utilities/
+addpath ../inputs/
+addpath ../inputs/Manoeuvre_Scripts/
+addpath ../post/
--- inputs/Settings_Scripts/LoadPaths.m
+++
@@ -1,8 +0,0 @@
-addpath ../../aero/
-addpath ../../functions/
-addpath ../../functions/objectives/
-addpath ../../functions/transformations/
-addpath ../../functions/utilities/
-addpath ../../inputs/
-addpath ../../inputs/Manoeuvre_Scripts/
-addpath ../../post/
