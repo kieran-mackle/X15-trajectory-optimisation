eb05e80a732856405c17a07511533ac4f53600c2
Kieran <kemackle98@gmail.com>
2021-11-08 15:52:18 +1000

    docs: added mapping functions starter files

---
+++ control/control_to_plant.m
@@ -0,0 +1,4 @@
+function output = control_to_plant(input)
+% Mapping function from control to plant models
+
+output = input;
---
+++ control/plant_to_control.m
@@ -0,0 +1,4 @@
+function output = plant_to_control(input)
+% Mapping function from plant to control models
+
+output = input;
