24340663c0f14b66e3abbbb307b40df315b15a8e
Kieran <kemackle98@gmail.com>
2021-11-10 14:24:37 +1000

    docs: added pitch-trim optimiser

---
+++ functions/utilities/trim_aero.m
@@ -0,0 +1,14 @@
+function aoa = trim_aero(auxdata, Ma, fda)
+% trim_aero returns the angle of attack to achieve pitch trim at a
+% specified Mach number and flap deflection angle.
+%
+% fda must be inputted in degrees.
+
+function absCm = GetAeroWrapper(aoa)
+    [~,~,Cm] = GetAero(auxdata, aoa, Ma, fda);
+    absCm = abs(Cm);
+end
+
+aoa = fminimax(@GetAeroWrapper, 0);
+
+end
