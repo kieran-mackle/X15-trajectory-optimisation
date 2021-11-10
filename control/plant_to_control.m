08339c7eb7ed5382ec42322ce34a42fa642a5e97
Kieran <kemackle98@gmail.com>
2021-11-10 13:18:36 +1000

    docs: starter code

--- control/plant_to_control.m
+++ control/plant_to_control.m
@@ -1,4 +1,32 @@
 function output = plant_to_control(input)
 % Mapping function from plant to control models
 
+% For tensor6 to cart3 dynamics models
+
+% State variables
+latd    = X(:,1);
+lond    = X(:,2);
+dist    = X(:,3);
+uD      = X(:,4);
+vD      = X(:,5);
+wD      = X(:,6);
+p       = X(:,7);
+q       = X(:,8);
+r       = X(:,9);
+m       = X(:,10);
+fda     = X(:,11);
+thr     = X(:,12);
+roll    = X(:,13);
+pitch   = X(:,14);
+yaw     = X(:,15);
+
+
+
+T_DG        = TM_DG(delta(i));
+T_BG        = TM_BG(roll(i),pitch(i),yaw(i));
+T_BD        = T_BG * T_DG';
+vBEB        = T_BD * vBED(i,:)';
+beta        = asin(vBEB(2)/norm(vBEB));
+aoa         = atan(vBEB(3)/vBEB(1));
+
 output = input;
--- control/single_update.m
+++ control/single_update.m
@@ -5,7 +5,6 @@
 % is to create a simple case to work towards integrating the 3DOF control
 % model into the 6DOF simulation control framework.
 
-
 % Add MPC controller directory
 addpath '/home/kieran/Documents/MATLAB/MPC'
 
@@ -32,6 +31,15 @@ out = output.result.solution.phase;
 x0  = out.state(1,:);
 u0  = out.control(1,:);
 
+
+
+% MAP STATE FROM 6DOF TO 3DOF MODEL
+lat = x0(1); lon = x0(2); dist = x0(3);
+uD = x0(4); vD = x0(5); wD = x0(6);
+roll = x0(13); pitch = x0(14); yaw = x0(15);
+
+
+
 initial.state = x0;
 initial.control = u0;
 
@@ -51,12 +59,10 @@ cost_weightings.output         = 1e3* [1, 0, 0, 0, 0;   % Altitude
                                        0, 0, 0, 0, 1];  % FPA
 cost_weightings.control        = eye(length(initial.control));
 
-
 % Quadprog constraint handling options
 constraints.type = 'soft';      % None, soft, hard or mixed
 penalty_method = 'linear';      % Quadratic or linear
-penalty_weight      = 1e3;
-
+penalty_weight = 1e3;
 
 constraints.hard.rate    = [-10*deg, 10*deg;
                             -0.2,   0.2];
@@ -89,7 +95,6 @@ constraints.weights.hard_output = [0, 0;
                                    0, 0;
                                    0, 0];
 
-
 % ----------------------------------------------------------------------- %
 % Construct MPC Input
 % ----------------------------------------------------------------------- %
@@ -101,8 +106,8 @@ mpc_input.penalty_weight    = penalty_weight;
 mpc_input.params            = params;
 mpc_input.initial           = initial;
 mpc_input.solver            = convex_solver;
-
-
+mpc_input.t                 = 0;                % From mpc_control.m
+mpc_input.reference = [20e3, 0, 0, 6, 0]';      % From X15_reference.m
 
 % Call MPC Update Solver
 U_k = mpc_update(mpc_input);
