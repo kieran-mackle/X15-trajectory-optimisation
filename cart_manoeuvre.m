0fb25dc2226d675d7a265bae85e4bc832e1700a7
Kieran <kemackle98@gmail.com>
2021-11-11 11:16:37 +1000

    docs: first iteration complete

--- cart_manoeuvre.m
+++ cart_manoeuvre.m
@@ -31,7 +31,7 @@ a       = sqrt(gamma.*R.*T);
 vN0     = 0;                    vNf     = 0;                % (m/s)
 vE0     = Ma0*a(1);             vEf     = Maf*a(2);         % (m/s)
 vD0     = 0;                    vDf     = 0;                % (m/s)
-m0      = 10e3;                 mf      = 10e3;
+m0      = 10e3;                 mf      = m0;               % (kg)
 
 fda0    = 0;                    fdaf    = 0;
 thr0    = 0;                    thrf    = 0;
@@ -48,16 +48,17 @@ hmin    = 10e3;                 hmax    = 50e3;             % (m)
 Mamin   = 4;                    Mamax   = 8;                % (-)
 aoamin  = -30*d2r;              aoamax  = -aoamin;          % (rad)
 
+fpa0    = 0*d2r;                fpaf    = 0*d2r;            % (rad)
+
 % POSITIONAL BOUNDS
 Nmin    = 0;                    Nmax    = 0;                % (m)
 Emin    = 0;                    Emax    = vEf*tmax;         % (m)
 Dmin    = -Re-hmax;             Dmax    = -Re-hmin;         % (m)
 
-% VELOCITY BOUNDS
+% VELOCITY BOUNDS  % TODO - relax velocity min/max
 vNmin   = 0;                    vNmax   = 0;                % (m)
 vEmin   = Mamin*a(0);           vEmax   = Mamax*a(2);         % (m)
-vDmin   = ;             vDmax   = ;         % (m)
-
+vDmin   = Mamin*a(0);           vDmax   = Mamax*a(2);         % (m)   
 mmin    = m0;                   mmax    = m0;               % (kg)
 
 fdamin  = -40*d2r;              fdamax  = -fdamin;          % (rad)
@@ -68,9 +69,8 @@ dthrmin = -0.2;                 dthrmax = -dthrmin;         % (N/s)
 
 % END OF USER INPUTS --------------------------------------------------- %
 % ---------------------------------------------------------------------- %
-sBE_Lmin = [Nmin, Emin, Dmin];          sBE_Lmax = [Nmax, Emax, Dmax];
-vBE_Lmin = [vNmin, vEmin, vDmin];       vBE_Lmax = [vNmax, vEmax, vDmax];
-
+sBE_Lmin = [Nmin, Emin, Dmin];      sBE_Lmax = [Nmax, Emax, Dmax];
+vBE_Lmin = [vNmin, vEmin, vDmin];   vBE_Lmax = [vNmax, vEmax, vDmax];
 
 
 
@@ -88,7 +88,7 @@ bounds.phase.initialstate.upper = [sBE_Lmax, vBE_Lmax, m0,  ...
 
 bounds.phase.state.lower        = [sBE_Lmin, vBE_Lmin, mmin,...
                                    fdamin, thrmin];
-bounds.phase.state.upper        = [sBE_Lmax, vBE_Lmax, m0,  ...
+bounds.phase.state.upper        = [sBE_Lmax, vBE_Lmax, mmax,  ...
                                    fdamax, thrmax];
 
 bounds.phase.finalstate.lower   = [sBE_Lmin, vBE_Lmin, mmin,...
@@ -96,15 +96,15 @@ bounds.phase.finalstate.lower   = [sBE_Lmin, vBE_Lmin, mmin,...
 bounds.phase.finalstate.upper   = [sBE_Lmax, vBE_Lmax, mmax,...
                                    fdamax, thrmax];
 
-bounds.phase.control.lower      = [dfdamin,dthrmin];
-bounds.phase.control.upper      = [dfdamax,dthrmax];
+bounds.phase.control.lower      = [dfdamin, dthrmin];
+bounds.phase.control.upper      = [dfdamax, dthrmax];
 
 bounds.phase.path.lower         = [aoamin, Mamin];
 bounds.phase.path.upper         = [aoamax, Mamax];
 
 % Eventgroups ---------------------------------------------------------- %
-bounds.eventgroup(1).lower      = [Ma0,Maf];
-bounds.eventgroup(1).upper      = [Ma0,Maf];
+bounds.eventgroup(1).lower      = [Ma0, Maf];
+bounds.eventgroup(1).upper      = [Ma0, Maf];
 
 bounds.eventgroup(2).lower      = [fpa0, fpaf];
 bounds.eventgroup(2).upper      = [fpa0, fpaf];
@@ -117,51 +117,30 @@ if imported_guess == 1
     guess.phase.state           = newguess.state;
     guess.phase.control         = newguess.control;
 else
-    N   = 100;
-    t   = linspace(t0,tf,N)';
-    h   = linspace(h0,hf,N)';
-    dist = linspace(dist0,distf,N)';
-    lon = linspace(lon0,lonf,N)';
-    lat = linspace(lat0,latf,N)';
-    V   = linspace(V0,Vf,N)';
-    hda = linspace(hda0,hdaf,N)';
-    fpa = linspace(fpa0,fpaf,N)';
-    m   = linspace(m0,mf,N)';
-    fda = linspace(fda0,fdaf,N)';
-    dfda = linspace(dfda0,dfdaf,N)';
-    thr = linspace(thr0,thrf,N)';
-    dthr = linspace(dthr0,dthrf,N)';
-
-    for i = 1:length(t)
-        delta       = GetDelta(h(i),lat(i));
-
-        T_DG        = TM_DG(delta);
-        sBED(i,:)   = [lat(i),lon(i),dist(i)];
-
-        vBEG        = pol2car(V(i),hda(i),fpa(i));
-        vBED(i,:)   = (T_DG * vBEG)';
-
-        wBIB(i,:)   = [p0,q0,r0];
-
-        Euls(i,:)   = [roll0,pitch0,yaw0];
-    end
+    steps   = 10;
+    t       = linspace(t0, tf, steps)';
+    N       = linspace(N0, Nf, steps);
+    E       = linspace(E0, Ef, steps);
+    D       = linspace(D0, Df, steps);
+    vN      = linspace(vN0, vNf, steps);
+    vE      = linspace(vE0, vEf, steps);
+    vD      = linspace(vD0, vDf, steps);
+    m       = linspace(m0,mf,steps)';
+    fda     = linspace(fda0,fdaf,steps)';
+    dfda    = linspace(dfda0,dfdaf,steps)';
+    thr     = linspace(thr0,thrf,steps)';
+    dthr    = linspace(dthr0,dthrf,steps)';
 
     guess.phase.time                = t;
-    guess.phase.state(:,1)          = sBED(:,1);
-    guess.phase.state(:,2)          = sBED(:,2);
-    guess.phase.state(:,3)          = sBED(:,3);
-    guess.phase.state(:,4)          = vBED(:,1);
-    guess.phase.state(:,5)          = vBED(:,2);
-    guess.phase.state(:,6)          = vBED(:,3);
-    guess.phase.state(:,7)          = wBIB(:,1);
-    guess.phase.state(:,8)          = wBIB(:,2);
-    guess.phase.state(:,9)          = wBIB(:,3);
-    guess.phase.state(:,10)         = m;
-    guess.phase.state(:,11)         = fda;
-    guess.phase.state(:,12)         = thr;
-    guess.phase.state(:,13)         = Euls(:,1);
-    guess.phase.state(:,14)         = Euls(:,2);
-    guess.phase.state(:,15)         = Euls(:,3);
+    guess.phase.state(:,1)          = N;
+    guess.phase.state(:,2)          = E;
+    guess.phase.state(:,3)          = D;
+    guess.phase.state(:,4)          = vN;
+    guess.phase.state(:,5)          = vE;
+    guess.phase.state(:,6)          = vD;
+    guess.phase.state(:,7)          = m;
+    guess.phase.state(:,8)          = fda;
+    guess.phase.state(:,9)          = thr;
 
     guess.phase.control(:,1)        = dfda;
     guess.phase.control(:,2)        = dthr;
--- functions/cartesian_objective.m
+++ functions/cartesian_objective.m
@@ -8,43 +8,28 @@ X0      = input.phase.initialstate;
 Xf      = input.phase.finalstate;
 auxdata = input.auxdata;
 
-N0      = X0(:,1);                  Nf      = Xf(:,1);
-E0      = X0(:,2);                  Ef      = Xf(:,2);
+Re      = auxdata.Re;
+
+% N0      = X0(:,1);                  Nf      = Xf(:,1);
+% E0      = X0(:,2);                  Ef      = Xf(:,2);
 D0      = X0(:,3);                  Df      = Xf(:,3);
-vN0     = X0(:,4);                  vNf     = Xf(:,4);
-vE0     = X0(:,5);                  vEf     = Xf(:,5);
-vD0     = X0(:,6);                  vDf     = Xf(:,6);
+% vN0     = X0(:,4);                  vNf     = Xf(:,4);
+% vE0     = X0(:,5);                  vEf     = Xf(:,5);
+% vD0     = X0(:,6);                  vDf     = Xf(:,6);
 
 % ----------------------------------------------------------------------- %
-Re0     = GetRe(lat0, ad.radius_model);
-Ref     = GetRe(latf, ad.radius_model);
-h0      = -(dist0 + Re0);
-hf      = -(distf + Ref);
+vBE_L0      = X0(:,4:6);            vBE_Lf  = Xf(:,4:6);
+[V0,~,fpa0] = car2pol(vBE_L0);      [Vf,~,fpaf] = car2pol(vBE_Lf);
+
+h           = [-D0; -Df]-Re;
+[T,~,~]     = atmospheric_model(h);
+a           = sqrt(gamma.*R.*T);
+V           = [V0, Vf];
+Ma          = V./a;
 
 % ----------------------------------------------------------------------- %
-delta0  = GetDelta(h0,lat0);       deltaf  = GetDelta(hf,latf);
-T_DG0   = TM_DG(delta0);           T_DGf   = TM_DG(deltaf);
-
-vBED0   = [uD0,vD0,wD0];
-vBEDf   = [uDf,vDf,wDf];
-
-vBEG0   = T_DG0' * vBED0';
-vBEGf   = T_DGf' * vBEDf';
-
-[V0,~,fpa0] = car2pol(vBEG0);
-[Vf,~,fpaf] = car2pol(vBEGf);
-
-[Temp,~,~] = GetAtmo(h0);
-sos0        = sqrt(ad.gamma*ad.R*Temp);
-Ma0 = V0/sos0;
-
-[Temp,~,~] = GetAtmo(hf);
-sosf = sqrt(ad.gamma*ad.R*Temp);
-Maf = Vf/sosf;
-
-% ----------------------------------------------------------------------- %
-output.eventgroup(1).event = [Ma0,Maf];
-output.eventgroup(2).event = [fpa0,fpaf];
+output.eventgroup(1).event = [Ma(1), Ma(2)];
+output.eventgroup(2).event = [fpa0, fpaf];
 
 J = tf;
     
