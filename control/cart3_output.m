fbd2d7e90fb64eb4d8677f8a98a6a7136d1046a1
Kieran <kemackle98@gmail.com>
2021-11-15 10:34:21 +1000

    docs: added cartesian control output function

---
+++ control/cart3_output.m
@@ -0,0 +1,24 @@
+function output = cart3_output(input)
+
+fda = input(8);
+thr = input(9);
+
+% Constants
+Re = geocradius(0); 
+R = 287.053;
+gamma = 1.4;
+
+% Models
+atmospheric_model = auxdata.atmospheric_model;
+
+sBE_L   = input(1:3);
+vBE_L   = input(4:6);
+
+h           = -sBE_L(:,3) - Re;
+[T,~,~]   = atmospheric_model(h);
+a           = sqrt(gamma.*R.*T);
+
+[V,~,fpa] = car2pol(vBE_L);
+Ma          = V/a;
+
+output = [h, fda, thr, Ma, fpa];
