8ba0d7e3b4c57cc5aacef9090edf3e739bb53185
Kieran <kemackle98@gmail.com>
2021-11-17 09:25:17 +1000

    docs: added initial mapping functions

--- control/plant_to_control.m
+++ control/plant_to_control.m
@@ -1,9 +1,14 @@
 function output = plant_to_control(input)
 % Mapping function from plant to control models
-
 % For tensor6 to cart3 dynamics models
 
-% State variables
+% Initialise output with original input
+output = input;
+
+X = input.initial.state;
+U = input.initial.control;
+
+% Extract state variables
 latd    = X(:,1);
 lond    = X(:,2);
 dist    = X(:,3);
@@ -20,13 +25,23 @@ roll    = X(:,13);
 pitch   = X(:,14);
 yaw     = X(:,15);
 
+% Map state inputs to cartesian state inputs
+North   = 0;    % arbitrary starting point
+East    = 0;    % arbitrary starting point
+Down    = dist; 
+vNorth = uD;
+vEast = vD;
+vDown = wD;
 
 
-T_DG        = TM_DG(delta(i));
-T_BG        = TM_BG(roll(i),pitch(i),yaw(i));
-T_BD        = T_BG * T_DG';
-vBEB        = T_BD * vBED(i,:)';
-beta        = asin(vBEB(2)/norm(vBEB));
-aoa         = atan(vBEB(3)/vBEB(1));
+mapped_state(1) = North;
+mapped_state(2) = East;
+mapped_state(3) = Down;
+mapped_state(4) = vNorth;
+mapped_state(5) = vEast;
+mapped_state(6) = vDown;
+mapped_state(7) = m;
+mapped_state(8) = fda;
+mapped_state(9) = thr;
 
-output = input;
+output.initial.state = mapped_state;
