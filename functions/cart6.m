33415f791319b1d36b44de180eaa5e2e47523cac
Kieran <kemackle98@gmail.com>
2021-11-18 10:42:00 +1000

    docs: coupled ODEs implemented

--- functions/cart6.m
+++ functions/cart6.m
@@ -7,10 +7,8 @@ function output = cart6(input)
 
 
 
-
-
-i=1;
+d_vBE_B(i,:) = f_sp_B - R_BE_B * vBE_B + T_BL * g_L;
 d_sBE_L(i,:) = T_BL' * vBE_B(i,:);
+d_wBE_B(i,:) = invMOI * (-R_BE_B*MOI*wBE_B(i,:)' + mBB);
 
-
-output.dynamics = [d_sBE_L];
+output.dynamics = [d_sBE_L, d_vBE_B];
