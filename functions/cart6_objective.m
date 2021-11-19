3fe5f1de005e40155e1fa0a0558f3b6cf29e664e
Kieran <kemackle98@gmail.com>
2021-11-19 14:24:19 +1000

    docs: created cart6 objective function

---
+++ functions/cart6_objective.m
@@ -0,0 +1,40 @@
+function output = cart6_objective(input)
+% =============== Altitude hold objective function ================ 
+% Generalised objective function for X-15 trajectory optimisation.
+% ================================================================= 
+
+tf      = input.phase.finaltime;
+% X0      = input.phase.initialstate;
+% Xf      = input.phase.finalstate;
+auxdata = input.auxdata;
+
+% Re      = auxdata.Re0;
+
+% N0      = X0(:,1);                  Nf      = Xf(:,1);
+% E0      = X0(:,2);                  Ef      = Xf(:,2);
+% D0      = X0(:,3);                  Df      = Xf(:,3);
+% vN0     = X0(:,4);                  vNf     = Xf(:,4);
+% vE0     = X0(:,5);                  vEf     = Xf(:,5);
+% vD0     = X0(:,6);                  vDf     = Xf(:,6);
+% vBE_B0      = X0(:,4:6);            vBE_Bf  = Xf(:,4:6);
+% vBE_B = [vBE_B0; vBE_Bf];
+
+% ----------------------------------------------------------------------- %
+% h           = [-D0; -Df]-Re;
+% [T,~,~]     = auxdata.atmospheric_model(h);
+% a           = sqrt(auxdata.gamma.*auxdata.R.*T);
+% V           = sqrt(sum(vBE_B.^2,2));
+% Ma          = V'./a;
+
+% ----------------------------------------------------------------------- %
+% output.eventgroup(1).event = [Ma(1), Ma(2)];
+% output.eventgroup(2).event = [fpa0, fpaf];
+
+
+if auxdata.altitude_hold == 1
+    J = -tf;
+else
+    J = tf;
+end
+
+output.objective = J;
