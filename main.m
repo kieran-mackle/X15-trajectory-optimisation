a80ce5f48f657de94a4e372464f653513afc736a
Kieran <kemackle98@gmail.com>
2021-12-02 10:47:46 +1000

    docs: attempt at climb manoeuvre

--- main.m
+++ main.m
@@ -23,9 +23,9 @@ auxdata.atmospheric_model = @(h)GetAtmo(h);
 % ----------------------------------------------------------------- %
 %                       Load manoeuvre script                       %
 %------------------------------------------------------------------ %
-manoeuvre_spec.type = 'hold';              % 'climb' / 'hold'
-manoeuvre_spec.name = '20km_hold';
-manoeuvre_spec.h0 = 20e3;
+manoeuvre_spec.type = 'climb';              % 'climb' / 'hold'
+manoeuvre_spec.name = '1520km_climb';
+manoeuvre_spec.h0 = 15e3;
 manoeuvre_spec.hf = 20e3;
 manoeuvre_spec.Ma0 = 6;
 manoeuvre_spec.Maf = 6;
@@ -41,7 +41,8 @@ manoeuvre_spec.use_guess = 0;
 
 
     % CARTESIAN 6DOF
-[bounds, guess, auxdata] = cart6_manoeuvre(manoeuvre_spec, auxdata);
+% [bounds, guess, auxdata] = cart6_manoeuvre(manoeuvre_spec, auxdata);
+[bounds, guess, auxdata] = cart6_climb(manoeuvre_spec, auxdata);
 % plot_bounds_and_guess(bounds, guess, auxdata)
 
 % ----------------------------------------------------------------- %
