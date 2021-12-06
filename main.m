d5ba6e392886803fa591a8ee14ecf011bf2f6217
Kieran <kemackle98@gmail.com>
2021-12-06 15:19:56 +1000

    docs: Euler post script

--- main.m
+++ main.m
@@ -95,5 +95,6 @@ t = output.result.solution.phase.time;
 % post6;
 % plot6;
 
-post = cart6_post(auxdata, output, t);
+% post = cart6_post(auxdata, output, t);
+post = euler_cart6_post(auxdata, output, t);
 plot_cart6(post)
